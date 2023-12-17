9ec0f757c029dc61d3192675fb3a3496decfee6c
Your Name <you@example.com>
2023-12-17 10:45:00 +0000

    fix connect new nodes on a vps

--- connect.sh
+++ connect.sh
@@ -5,15 +5,12 @@ if [ -f .env ]; then
     export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 fi
 
-for vps in $VPSS
+echo "Installing $VPS"
+for (( port=$PORT_FROM; port<=$PORT_TO; port++ ))
 do
-    echo "Installing $vps"
-    for (( port=$PORT_FROM; port<=$PORT_TO; port++ ))
-    do
-    echo "Connect redis on $vps:$port"
-    redis-cli -p $PORT_FROM -a $REDIS_PASSWORD -c cluster meet $vps $port
-    done
+echo "Connect redis on $VPS:$port"
+redis-cli -p $PORT_FROM -a $REDIS_PASSWORD -c cluster meet $VPS $port
 done
 
-redis-cli -p 7000 -a $REDIS_PASSWORD -c cluster nodes
+# redis-cli -p 7000 -a $REDIS_PASSWORD -c cluster nodes
  
